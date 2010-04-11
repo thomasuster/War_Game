46498d810d11bc55d455b0989184bd4ce5707567
Thomas Uster <thomasuster@gmail.com>
2010-04-11 15:12:40 -0700

    Going to be adding players tables

--- back/app/controllers/user_controller.rb
+++ back/app/controllers/user_controller.rb
@@ -1,20 +1,20 @@
 class UserController < ApplicationController
 
-  def login
-  
-    if request.post?
-      user = User.authenticate(params[:email], params[:password])
-      if user
-        session[:user_uuid] = user.uuid
-        redirect_to(:action => "index")
-      else
-        flash.now[:notice] = "Invalid user/password combination"
-      end
-    end
-  end
-  
+	def login
+	
+		if request.post?
+			user = User.authenticate(params[:email], params[:password])
+			if user
+				session[:user_uuid] = user.uuid
+				redirect_to(:action => "index")
+			else
+				flash.now[:notice] = "Invalid user/password combination"
+			end
+		end
+	end
+	
+	# Returns the game uuid, game name and map name	
 	def show_games
-		#Returns the game uuid, game name and map name
 		game = Game.new
 		games = game.get_games
 
@@ -27,23 +27,22 @@ class UserController < ApplicationController
 		end
 		@games = h
 	end
-  
-  
-  
+	
+	#Trys to join a game
 	def join
-
+		params[:game_uuid]
 	end
-  
-  def logout
-    session[:user_uuid] = nil
-    flash[:notice] = "Logged out"
-    redirect_to(:action => "login")
-  end
-  
 
-  
-  def index
-    #@total_orders = Order.count
-  end
+	def logout
+		session[:user_uuid] = nil
+		flash[:notice] = "Logged out"
+		redirect_to(:action => "login")
+	end
+	
+
+	
+	def index
+		#@total_orders = Order.count
+	end
 
 end

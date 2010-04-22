c660b56c894be5dd3671a27736214efe7efa4fcd
Thomas Uster <thomasuster@gmail.com>
2010-04-21 20:15:26 -0700

    map inserting from form input

--- back/app/controllers/front_controller.rb
+++ back/app/controllers/front_controller.rb
@@ -8,10 +8,28 @@ class FrontController < ApplicationController
 	
 	def save
 		#@data = request.env['RAW_POST_DATA']
-		@data = params
-		print "\n\n\n\n\n"
-		print params["map_name"] + "\n"
-		print params["num_players"]
+		#@data = params
+		params[:map] = Hash.new
+		params[:map][:name] = params["name"]
+		params[:map][:players] = params["players"]
+		params[:map][:data] = params["data"]
+		
+		if true
+			@map = Map.new(params[:map])
+			
+			if @map.save
+				@response = "Map added to database.";
+			else
+				@response = "Map failed to be added to database.";
+			end
+		end
+		
+		#@response = "Map added to database.";
+		#@response = params["name"];
+		#print "\n\n\n\n\n"
+		#print params["map_name"] + "\n"
+		#print params["num_players"]
 		render :layout => false
+		
 	end
 end

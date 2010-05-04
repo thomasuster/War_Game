5df09e4c6bf939d9f44b2dac80930ec5b9a120f9
Thomas Uster <thomasuster@gmail.com>
2010-05-03 17:04:48 -0700

    Stats working with Sector, also unit test

--- back/lib/war_game/Sector.rb
+++ back/lib/war_game/Sector.rb
@@ -1,26 +1,41 @@
+require 'Board_object'
+require 'Stats'
+
 class Sector < Board_object
 	attr_accessor :sector_name
+	STATS_XML = "../../public/front/stats/sectors.xml"
 	
 	def initialize(sector_name, location)
 		super(location)
 		@sector_name = sector_name
 		
 		#Static Stats
-		if (stats == nil)
-			stats = new Stats(stats_xml);
-			sector_stats = stats.hash;
+		if defined?(@@stats) == nil
+			@@stats = Stats.new(STATS_XML).hash
 		end
 	end
 	
-	def get_name() { return sector_name; }
-			
-	def get_infantry_moves() { return sector_stats[sector_name]["infantry_moves"]; }
+	def get_name() 
+		return sector_name;
+	end
 		
-	def get_vehicle_moves() { return int(sector_stats[sector_name]["vehicle_moves"]); }
-		
-	def get_attacking() { return int(sector_stats[sector_name]["attacking"]); }
-		
-	def get_defending() { return int(sector_stats[sector_name]["defending"]); }
-		
-	def to_s() { return sector_name; }
+	def get_infantry_moves() 
+		return @@stats[sector_name]["infantry_moves"]
+	end
+
+	def get_vehicle_moves() 
+		return @@stats[sector_name]["vehicle_moves"]
+	end
+
+	def get_attacking() 
+		return @@stats[sector_name]["attacking"]
+	end
+
+	def get_defending() 
+		return @@stats[sector_name]["defending"]
+	end
+
+	def to_s() 
+		return sector_name;
+	end
 end
